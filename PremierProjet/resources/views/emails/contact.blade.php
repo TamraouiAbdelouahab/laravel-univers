2b949caf1adcea90fdf84a41565f5f2dcc46a6af
TamraouiAbdelouahab <abdelwahabtamraoui@gmail.com>
2024-10-22 23:53:26 +0100

    Ajouter les Routes et Controllers

---
+++ PremierProjet/resources/views/emails/contact.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Email Test</title>
+</head>
+<body>
+    <h1>{{ $name }}</h1>
+    <p>je suis développeur</p>
+</body>
+</html>
--- PremierProjet/routes/web.php
+++ PremierProjet/routes/web.php
@@ -1,11 +1,39 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
+// -----------------------------------
+
+use Illuminate\Support\Facades\Mail;
+
+Route::get('/send-email', function () {
+    $details = [
+        'title' => 'Mail de test depuis Laravel',
+        'body' => 'Ceci est un test d\'envoi d\'email.'
+    ];
+
+    Mail::send([], [], function($message) use ($details) {
+        $message->to('destinataire@example.com', 'Nom Destinataire')
+                ->subject('Test d\'envoi d\'email')
+                ->html($details['body']);
+            });
+
+    return 'E-mail envoyé avec succès !';
+});
+
+// --------------------------------
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/emails', function () {
+    return view('emails.contact');
 });
 
 Route::get('/about', function () {
     return view('about');
 });
+
+Route::get('/contact',[ContactController::class,'create']);
+Route::post('/contact',[ContactController::class,'store']);
